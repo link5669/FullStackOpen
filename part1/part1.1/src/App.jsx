dad38c71ac5f1b3b9de5c8d04beb689a2db52b26
link5669 <milesacq@icloud.com>
2022-12-26 17:15:56 -0500

    part2, react hooks and stateful values

--- part1/part1.1/src/App.jsx
+++ part1/part1.1/src/App.jsx
@@ -1,7 +1,9 @@
-const Content = ({part1, exercises1}) => {
+const Content = (props) => {
   return (
   <p>
-      {part1} {exercises1}
+      {props.parts[0].name} {props.parts[0].exercises}<br/>
+      {props.parts[1].name} {props.parts[1].exercises}<br/>
+      {props.parts[2].name} {props.parts[2].exercises}<br/>
     </p>
   );
 };
@@ -14,28 +16,36 @@ const Header = ({course}) => {
   );
 };
 
-const Total = ({exercises1, exercises2, exercises3}) => {
+const Total = (props) => {
   return (
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   );
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content part1={part1} exercises1={exercises1}></Content>
-      <Content part1={part2} exercises1={exercises2}></Content>
-      <Content part1={part3} exercises1={exercises3}></Content>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}></Total>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
