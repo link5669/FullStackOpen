0903b2145f4afa651fd079026a985fecc7d93522
link5669 <milesacq@icloud.com>
2022-12-30 15:47:30 -0500

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,6 +4,9 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '609-111-1111' }
   ]) 
+  const [allPersons, setAllPersons] = useState([
+    { name: 'Arto Hellas', number: '609-111-1111' }
+  ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
@@ -18,6 +21,7 @@ const App = () => {
       }
     }
     if (!flag) {
+      setAllPersons(persons.concat(newObj))
       setPersons(persons.concat(newObj))
     } else {
       alert(`${newObj.name} is already present!`)
@@ -31,11 +35,24 @@ const App = () => {
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value)
   }
+  
+  const handleSearchChange = (e) => {
+    setPersons(allPersons.filter(person => {
+      let temp = person.name.toLowerCase();
+      let temp2 = e.target.value.toLowerCase();
+      return temp.includes(temp2)
+    }));
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <h3>Search by name</h3>
+        <input onChange={handleSearchChange}></input>
+      </form>
       <form onSubmit={addPerson}>
+        <h3>Add person</h3>
         <div>
           name: <input 
           onChange={handleNameChange}
