08090cd28424d6ed3a6df2dfdc900639bdb592c8
link5669 <milesacq@icloud.com>
2023-01-07 22:28:48 -0500

    eslint

--- part3/node/index.js
+++ part3/node/index.js
@@ -4,7 +4,6 @@ const app = express()
 app.use(express.static('build'))
 app.use(express.json())
 const cors = require('cors')
-const { json } = require('express')
 app.use(cors())
 
 const password = '56695669'
@@ -25,18 +24,18 @@ const phonebookSchema = new mongoose.Schema({
       validator: function(v) {
         let numDash = 0
         for (let i = 0; i < v.length; i++) {
-          if (v.charAt(i) == '-') {
-            if (i == 2 || i == 3) {
+          if (v.charAt(i) === '-') {
+            if (i === 2 || i === 3) {
               numDash += 1
             } else {
               return false
             }
-          } else if (numDash == 0 || numDash == 1) {
+          } else if (Number(numDash) === 0 || Number(numDash) === 1) {
             if (v.charAt(i).isNAN) {
               return false
             }
           } else {
-            return false;
+            return false
           }
         }
         return true
@@ -49,19 +48,19 @@ const phonebookSchema = new mongoose.Schema({
 
 app.post('/api/persons', (request, response, next) => {
   if (!request.body.name || !request.body.number) {
-      response.json("Must include name and number!")
-      return
+    response.json('Must include name and number!')
+    return
   }
   const newPerson = new phonebookEntry({
-      name: request.body.name,
-      number: request.body.number,
-      id: Math.floor(Math.random() * 1000)
+    name: request.body.name,
+    number: request.body.number,
+    id: Math.floor(Math.random() * 1000)
   })
   newPerson.save()
-  .then(update => response.json(update))
-  .catch(error => {
-    next(error)
-  })
+    .then(update => response.json(update))
+    .catch(error => {
+      next(error)
+    })
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -86,61 +85,56 @@ phonebookSchema.set('toJSON', {
 
 const phonebookEntry = mongoose.model('PhonebookEntry', phonebookSchema)
 
-async function findAll(response) {
-  await phonebookEntry.find({}).then(result => {
-    response.json(result)
-  })
-}
-
 app.put('/api/persons/:id', (request, response, next) => {
   const updatedEntry = {
     name: request.body.name,
     number: request.body.number,
     id: request.body.id
   }
-  phonebookEntry.findByIdAndUpdate(request.body.id, updatedEntry, {new: true})
+  phonebookEntry.findByIdAndUpdate(request.body.id, updatedEntry, { new: true })
     .then(updatedEntry => {
       response.json(updatedEntry)
     }).catch(error => next(error))
 })
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World! Persons at <a href="/api/persons">this</a> link</h1>')
-  })
+  response.send('<h1>Hello World! Persons at <a href="/api/persons">this</a> link</h1>')
+})
 
-  app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people<br>${new Date()}`)
-  })
+app.get('/info', (request, response) => {
+  let numPeople = phonebookEntry.count({})
+  response.send(`Phonebook has info for ${numPeople} people<br>${new Date()}`)
+})
 
-  app.delete('/api/persons/:id', (request, response, next) => {
-    phonebookEntry.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    }).catch(error => next(error))
+app.delete('/api/persons/:id', (request, response, next) => {
+  phonebookEntry.findByIdAndRemove(request.params.id)
+    .then(response.status(204).end()).catch(error => next(error))
+})
+
+app.get('/api/persons', (request, response) => {
+  phonebookEntry.find({}).then(result => {
+    response.json(result)
   })
-  
-  app.get('/api/persons', (request, response) => {
-    phonebookEntry.find({}).then(result => {
+})
+
+
+
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  phonebookEntry.findById(id).then(result => {
+    if (result) {
       response.json(result)
-    })
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => {
+    next(error)
   })
+})
 
+// eslint-disable-next-line no-undef
+const PORT =  process.env.PORT || 3001
 
-
-  app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    const person = phonebookEntry.findById(id).then(result => {
-      if (result) {
-        response.json(result)
-      } else {
-        response.status(404).end()
-      }
-    }).catch(error => {
-      next(error)
-    })
-  })
-  
-  const PORT =  process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
