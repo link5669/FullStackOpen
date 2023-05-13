4ded2590da3c57110a2736e5e57357b7640bb857
link5669 <milesacq@icloud.com>
2023-05-12 23:39:07 -0400

    8.2/8.3

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -98,16 +98,37 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+  }
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
     Query: {
         bookCount: () => books.length,
-        authorCount: () => authors.length
+        authorCount: () => authors.length,
+        allBooks: () => books,
+        allAuthors: () => authors,
+    },
+    Author: {
+        bookCount: (root) => {
+            return books.filter(book => book.author === root.name).length
+        }
     }
 }
 
